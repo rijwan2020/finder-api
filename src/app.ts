968b8b404767d51a31819d783291f9241b051bd8
Rijwan <7rijwan1998@gmail.com>
2025-07-05 15:02:20 +0700

    use folder routes in app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import { Elysia } from "elysia";
 import { sequelize } from "@lib/db";
+import { folderRoute } from "@routes/folderRoute";
 
-const app = new Elysia();
+const app = new Elysia()
+    .use(folderRoute);
 
-const port = process.env.API_PORT || 4000;
+const port = process.env.API_PORT || 3000;
 
 await sequelize.sync({ alter: true });
 
