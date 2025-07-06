1089c2bed6157ba96e4da96b4c090885c0d52e1a
Rijwan <7rijwan1998@gmail.com>
2025-07-06 23:30:13 +0700

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import { Elysia } from "elysia";
 import { sequelize } from "@lib/db";
 import { finder } from "@routes/finder.route";
+import { cors } from "@elysiajs/cors";
 
 const app = new Elysia()
+    .use(cors({
+        origin: '*', // ✅ atau spesifik: origin: 'http://localhost:5173'
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    }))
     .use(finder);
 
 const port = process.env.API_PORT || 3000;
